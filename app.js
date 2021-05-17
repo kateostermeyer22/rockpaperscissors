30e8ad53764a4ca888d6f765f493d0109b383241
kateostermeyer22 <81381747+kateostermeyer22@users.noreply.github.com>
2021-05-17 10:56:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -12,45 +12,6 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
   userChoice = e.target.id
   userChoiceDisplay.innerHTML = userChoice
   generateComputerChoice()
-  getResult()
-}))
+  }))
 
-function generateComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
-  
-  if (randomNumber === 1) {
-    computerChoice = 'rock'
-  }
-  if (randomNumber === 2) {
-    computerChoice = 'scissors'
-  }
-  if (randomNumber === 3) {
-    computerChoice = 'paper'
-  }
-  computerChoiceDisplay.innerHTML = computerChoice
-}
 
-function getResult() {
-  if (computerChoice === userChoice) {
-    result = 'its a draw!'
-  }
-  if (computerChoice === 'rock' && userChoice === "paper") {
-    result = 'you win!'
-  }
-  if (computerChoice === 'rock' && userChoice === "scissors") {
-    result = 'you lost!'
-  }
-  if (computerChoice === 'paper' && userChoice === "scissors") {
-    result = 'you win!'
-  }
-  if (computerChoice === 'paper' && userChoice === "rock") {
-    result = 'you lose!'
-  }
-  if (computerChoice === 'scissors' && userChoice === "rock") {
-    result = 'you win!'
-  }
-  if (computerChoice === 'scissors' && userChoice === "paper") {
-    result = 'you lose!'
-  }
-  resultDisplay.innerHTML = result
-}
